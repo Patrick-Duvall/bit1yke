8733a46ed2f744206094707f08f81dd722411358
Patrick Duvall <duvallp208@gmail.com>
2021-03-14 16:41:27 -0600

    Add buildpack to app.json

--- bin/heroku/postdeploy.sh
+++ bin/heroku/postdeploy.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-echo $HEROKU_APP_NAME
-# export DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com
 heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com
-echo $DOMAIN_NAME
+echo "Setting DOMAIN_NAME to $DOMAIN_NAME"
