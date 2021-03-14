7b1432291c5e128ab12a022b3b4357e959125c55
Patrick Duvall <duvallp208@gmail.com>
2021-03-14 14:53:13 -0600

    Change postdeploy script

--- bin/heroku/postdeploy.sh
+++ bin/heroku/postdeploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo $HEROKU_APP_NAME
-echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com
+export DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com
+# heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com
+echo $DOMAIN_NAME
