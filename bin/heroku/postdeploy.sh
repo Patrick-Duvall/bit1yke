5ab86b8b8f1b9e4dcaedf860979e7620d553c632
Patrick Duvall <duvallp208@gmail.com>
2021-03-14 17:43:05 -0600

    Add app flag

--- bin/heroku/postdeploy.sh
+++ bin/heroku/postdeploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo '!!!'
-heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com
+heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com --app $HEROKU_APP_NAME
 echo "Setting DOMAIN_NAME to $DOMAIN_NAME"
