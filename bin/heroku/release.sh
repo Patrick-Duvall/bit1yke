86ce7ec85a0fcf4734146ec9ffbe0902682dcd13
Patrick Duvall <duvallp208@gmail.com>
2021-03-14 11:50:18 -0600

    Change release.sh

--- bin/heroku/release.sh
+++ bin/heroku/release.sh
@@ -1,8 +1,2 @@
-psql $DATABASE_URL -c 'SELECT id FROM users LIMIT 1'
-
-if [ $? == 0 ]; then
   echo "Running Rails DB migrations..."
   bundle exec rake db:migrate
-else
-  echo 'Skipping Rails DB migrations!!'
-fi
