f83803f8f1cf5bad51f3632b9bd01e24317e065a
Patrick Duvall <duvallp208@gmail.com>
2021-03-14 18:15:52 -0600

    Change postdeploy

--- bin/heroku/postdeploy.sh
+++ bin/heroku/postdeploy.sh
@@ -2,5 +2,6 @@
 
 echo '!!!'
 HEROKU_API_KEY=`heroku authorizations:create --expires-in 600 --short`
-heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com --app $HEROKU_APP_NAME
+heroku run "HEROKU_API_KEY=`heroku authorizations:create --expires-in 600 --short` heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com --app $HEROKU_APP_NAME" -a $HEROKU_APP_NAME
+# heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com --app $HEROKU_APP_NAME
 echo "Setting DOMAIN_NAME to $DOMAIN_NAME"
