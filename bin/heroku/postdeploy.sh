6818d9d52ef87847aca52749f407fb3f280ead21
Patrick Duvall <duvallp208@gmail.com>
2021-03-14 18:25:48 -0600

    Try setting in production.rb

--- bin/heroku/postdeploy.sh
+++ bin/heroku/postdeploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-echo '!!!'
-HEROKU_API_KEY=`heroku authorizations:create --expires-in 600 --short`
-heroku run "HEROKU_API_KEY=`heroku authorizations:create --expires-in 600 --short` heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com --app $HEROKU_APP_NAME" -a $HEROKU_APP_NAME
-# heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com --app $HEROKU_APP_NAME
-echo "Setting DOMAIN_NAME to $DOMAIN_NAME"
+# echo '!!!'
+# HEROKU_API_KEY=`heroku authorizations:create --expires-in 600 --short`
+# heroku run "HEROKU_API_KEY=`heroku authorizations:create --expires-in 600 --short` heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com --app $HEROKU_APP_NAME" -a $HEROKU_APP_NAME
+# # heroku config:set DOMAIN_NAME=https://$HEROKU_APP_NAME.herokuapp.com --app $HEROKU_APP_NAME
+# echo "Setting DOMAIN_NAME to $DOMAIN_NAME"
+
+  # "scripts": {
+  #   "postdeploy": "./bin/heroku/postdeploy.sh"
+  # },
